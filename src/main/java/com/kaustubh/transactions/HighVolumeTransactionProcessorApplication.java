91034470c658e1eb85be00d9a1e60470caca0169
Kaustubh Alandkar <kaustubhalandkar@gmail.com>
2025-08-11 10:12:00 +0530

    init: bootstrap high-volume-transaction-processor from Spring Initializr

---
+++ src/main/java/com/kaustubh/transactions/HighVolumeTransactionProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.kaustubh.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighVolumeTransactionProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighVolumeTransactionProcessorApplication.class, args);
+	}
+
+}
